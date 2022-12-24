1f9dc38f854b745d2d3c700981d816aa62c1b3b9
Shegda Igor <igorbeef@gmail.com>
2022-12-24 12:31:08 +0200

    profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
+import css from 'components/App.module.css';
+import { Profile } from './Profile/Profile.jsx';
+import user from '../user.json';
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template
+    <div className={css.cardApp}>
+      <Profile
+        key={user.username}
+        avatar={user.avatar}
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
     </div>
   );
 };
